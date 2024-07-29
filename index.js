7cf099fe3692eaae8a226ae6c238f779e719f313
Wasim Zaman <wasimxaman13@gmail.com>
2024-07-29 00:38:54 -0700

    chore: Update endpoint for estimation requests and improve code readability

--- index.js
+++ index.js
@@ -55,9 +55,10 @@ const sendEmail = (mailOptions, res) => {
 };
 
 // Endpoint for estimation requests
-app.get("/wasim", (req, res, next) => {
+app.get("/", (req, res, next) => {
   res.send("hello");
 });
+
 app.post("/sendEmail", (req, res) => {
   const { name, phoneNumber, projectDescription, city, whatsappNumber } =
     req.body;
