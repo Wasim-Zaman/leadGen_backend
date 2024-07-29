0f9e079a65ed1c05953601c1c50ac9a7a407cef6
Wasim Zaman <wasimxaman13@gmail.com>
2024-07-29 00:26:10 -0700

    chore: Refactor email sending logic and improve code readability

--- index.js
+++ index.js
@@ -10,9 +10,7 @@ const app = express();
 const port = 3001;
 
 // Constants
-const FROM_EMAIL = process.env.FROM_EMAIL;
-const PASS = process.env.PASS;
-const TO_EMAIL = process.env.TO_EMAIL;
+const { FROM_EMAIL, PASS, TO_EMAIL } = process.env;
 
 // Middleware setup
 app.use(bodyParser.json());
@@ -24,7 +22,7 @@ const storage = multer.diskStorage({
     cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, `${Date.now()}${path.extname(file.originalname)}`);
   },
 });
 
@@ -39,43 +37,58 @@ const transporter = nodemailer.createTransport({
   },
 });
 
+// Helper function to send emails
+const sendEmail = (mailOptions, res) => {
+  transporter.sendMail(mailOptions, (error) => {
+    if (error) {
+      return res.status(500).json({
+        status: "error",
+        message: "Failed to send email",
+        error,
+      });
+    }
+    return res.status(200).json({
+      status: "success",
+      message: "Email sent successfully",
+    });
+  });
+};
+
 // Endpoint for estimation requests
 app.post("/sendEmail", (req, res) => {
   const { name, phoneNumber, projectDescription, city, whatsappNumber } =
     req.body;
 
-  // Set up email data
   const mailOptions = {
     from: FROM_EMAIL,
     to: TO_EMAIL,
     subject: `New Estimation Request from ${name}`,
-    text: `Name: ${name}\nPhone Number: ${phoneNumber}\nProject Description: ${projectDescription}\nCity: ${city}\nWhatsApp Number: ${whatsappNumber}`,
+    text: `
+      Name: ${name}
+      Phone Number: ${phoneNumber}
+      Project Description: ${projectDescription}
+      City: ${city}
+      WhatsApp Number: ${whatsappNumber}
+    `,
   };
 
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res
-        .status(500)
-        .json({ status: "error", message: "Failed to send email", error });
-    }
-    res
-      .status(200)
-      .json({ status: "success", message: "Email sent successfully" });
-  });
+  sendEmail(mailOptions, res);
 });
 
 // Endpoint for career applications
 app.post("/apply", upload.single("resume"), (req, res) => {
   const { name, mobile, previousLinks } = req.body;
-  const resume = req.file;
+  const { file: resume } = req;
 
-  // Set up email data
   const mailOptions = {
-    from: FROM_EMAIL, // Replace with your email
-    to: TO_EMAIL, // Replace with the recipient's email
+    from: FROM_EMAIL,
+    to: TO_EMAIL,
     subject: `New Job Application from ${name}`,
-    text: `Name: ${name}\nMobile: ${mobile}\nPrevious Work Links: ${previousLinks}`,
+    text: `
+      Name: ${name}
+      Mobile: ${mobile}
+      Previous Work Links: ${previousLinks}
+    `,
     attachments: [
       {
         filename: resume.filename,
@@ -84,27 +97,16 @@ app.post("/apply", upload.single("resume"), (req, res) => {
     ],
   };
 
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res
-        .status(500)
-        .json({ status: "error", message: "Failed to send email", error });
-    }
-    res
-      .status(200)
-      .json({ status: "success", message: "Email sent successfully" });
-  });
+  sendEmail(mailOptions, res);
 });
 
 // Endpoint for consultation requests
 app.post("/sendConsultation", (req, res) => {
   const { name, email, country, phone, company, project, marketing } = req.body;
 
-  // Set up email data
   const mailOptions = {
-    from: FROM_EMAIL, // Replace with your email
-    to: TO_EMAIL, // Replace with the recipient's email
+    from: FROM_EMAIL,
+    to: TO_EMAIL,
     subject: `New Consultation Request from ${name}`,
     text: `
       Full Name: ${name}
@@ -117,17 +119,7 @@ app.post("/sendConsultation", (req, res) => {
     `,
   };
 
-  // Send email
-  transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      return res
-        .status(500)
-        .json({ status: "error", message: "Failed to send email", error });
-    }
-    res
-      .status(200)
-      .json({ status: "success", message: "Email sent successfully" });
-  });
+  sendEmail(mailOptions, res);
 });
 
 // Start the server
